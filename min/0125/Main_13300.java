ec4e29d77f156a75e863bd55071f7c6ec9fc0bc3
Min-Chae <kbpch02@naver.com>
2025-01-26 21:05:00 +0900

    일곱 난쟁이, 방 배정

---
+++ min/0125/Main_13300.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		int K = sc.nextInt();
+		int[] room = new int[12];
+		int answer = 0;
+		
+		for(int i = 0; i < N; i++) {
+			int tmp = (sc.nextInt() * 6 + sc.nextInt() - 1);
+			room[tmp] += 1;
+			if(room[tmp] > K || room[tmp] == 1) {
+				room[tmp] = 1;
+				answer += 1;
+			}
+		}
+		System.out.println(answer);
+	}
+}
+	
