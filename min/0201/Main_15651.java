4b1ca73b2e7e627925860f3cde3b4d399fe6dd97
Min-Chae <kbpch02@naver.com>
2025-02-01 21:30:20 +0900

    [백준] N과 M (1), N과 M (2), N과 M (3), N과 M (4)

---
+++ min/0201/Main_15651.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+	static int N, M;
+	static int[] temp;
+	static StringBuilder sb = new StringBuilder();
+	
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);      
+        N = sc.nextInt();
+        M = sc.nextInt();
+        sc.close();
+        
+        temp = new int[M];
+         
+        dfs(1, 0);
+        System.out.println(sb);
+    }
+    
+    static void dfs(int index, int depth) {
+    	if(depth == M) {
+    		for(int i = 0; i < M; i++) {
+    			sb.append(temp[i] + " ");
+    		}
+    		sb.append("\n");
+    		return;
+    	}
+    	
+    	for(int i = index; i <= N; i++) {
+    		temp[depth] = i;
+    		dfs(index, depth + 1);
+    	}
+    }
+}
